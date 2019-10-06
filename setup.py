00f367aeca818c78a3e82bd8f8dc72770df2f8d8
SitiSchu <admin@sitischu.com>
2019-10-06 04:04:22 +0200

    Add setup.py
    Signed-off-by: SitiSchu <admin@sitischu.com>

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import setuptools
+
+with open('requirements.txt') as f:
+    dependencies = [l.strip() for l in f]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='spamwatch',
+    version='0.0.0',
+    description='SpamWatch API Wrapper',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+    author='Simon Schürrle',
+    author_email='simon@spamwat.ch',
+    url='https://github.com/SpamWatch/spamwatch-py',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Typing :: Typed'
+    ],
+    install_requires=dependencies,
+    python_requires='>=3.6'
+)
