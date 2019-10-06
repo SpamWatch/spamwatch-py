f0b2e6b56e6548b23366405100a2eb01e729f8b6
SitiSchu <admin@sitischu.com>
2019-10-06 04:12:30 +0200

    Fix lint.sh
    Signed-off-by: SitiSchu <admin@sitischu.com>

--- lint.sh
+++ lint.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 echo "mypy"
-mypy --config-file=mypy.ini kantek
+mypy --config-file=mypy.ini spamwatch
 
 echo -e "\nflake8"
-flake8 --config=.flake8 kantek
+flake8 --config=.flake8 spamwatch
 
 echo -e "\npylint"
-pylint --rcfile=.pylintrc kantek
+pylint --rcfile=.pylintrc spamwatch
