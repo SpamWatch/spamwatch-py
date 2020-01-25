9d114ed7b49b6583f035baf63dbad01d2dd961b4
SitiSchu <admin@sitischu.com>
2020-01-25 02:58:13 +0100

    [2020-01-25] Bump version: 0.1.1 → 0.2.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='spamwatch',
-    version='0.1.1',
+    version='0.2.0',
     description='SpamWatch API Wrapper',
     long_description=long_description,
     long_description_content_type="text/markdown",
